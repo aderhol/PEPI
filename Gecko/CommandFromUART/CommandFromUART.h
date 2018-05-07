1fadcd1f6400001a92fcbbb70a3d4c36f6d29d94
Adam <a.e.hollos@gmail.com>
2018-05-07 21:33:11 +0200

    fix CommandFromUART header

--- Gecko/CommandFromUART/CommandFromUART.h
+++ Gecko/CommandFromUART/CommandFromUART.h
@@ -11,7 +11,7 @@ typedef enum {
 }Status;
 
 extern void CommandFromUARTInit(void);
-extern bool CommandFromUARTInitGet_status(Status* status);
-extern bool CommandFromUARTInitGet_reply(char** reply, int_fast16_t maxLength); //sets reply to NULL, if maxLength is too small
+extern bool CommandFromUARTGet_status(Status* status);
+extern bool CommandFromUARTGet_reply(char** reply, int_fast16_t maxLength); //sets reply to NULL, if maxLength is too small
 
 #endif
